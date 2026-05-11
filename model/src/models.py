880e2f177d6130622adcbfb2a070497d64a96382
NhatQuangDo <nquang.work@gmail.com>
2026-05-11 15:33:17 +1000

    Updating models for SKlearn appropriateness and editing notebooks

--- model/src/models.py
+++ model/src/models.py
@@ -1,102 +1,107 @@
 # Import Necessary libraries
-import matplotlib.pyplot as plt
 import numpy as np
-from tensor.keras.models import Sequential
-from tensor.keras.layers import Embedding, LSTM, Dense, Dropout
-from tensor.keras.losses import BinaryCrossentropy
-from tensor.keras.optimizers import Adam
+from sklearn.base import BaseEstimator, ClassifierMixin
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout, Input
+from tensorflow.keras.metrics import AUC
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.optimizers import Adam
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import classification_report, confusion_matrix, roc_curve, roc_auc_score, ConfusionMatrixDisplay
 
 class LSTM_model:
-    def __init__(self, input_dim= None, output_dim = 45, input_length= 100, dropout_rate= 0.2):
-        self.model = Sequential([
-            Embedding(input_dim=input_dim, output_dim=output_dim, input_length=input_length),
-            LSTM(64, input_shape=(input_length, output_dim), activation='relu', return_sequences=True),
-            Dropout(dropout_rate),
-            LSTM(64, activation='relu'),
-            Dropout(dropout_rate),
-            Dense(32, activation='relu'),
-            Dropout(dropout_rate),
-            Dense(1, activation='sigmoid')
-        ])
-    
-    def _compile(self, lr =0.01):
-        self.model.compile(loss = BinaryCrossentropy(), optimizer =Adam(learning_rate=lr), metrics=['auc'])
+    def __init__(self, input_dim= None, output_dim = 45, input_length= 100, lstm_units= [64, 32], 
+                 dense_units= [32], dropout_rate= 0.2, lr= 0.001, epochs= 10, batch_size= 32, random_state= 1211):
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.input_length = input_length
+        self.lstm_units = lstm_units
+        self.dense_units = dense_units
+        self.dropout_rate = dropout_rate
+        self.lr = lr
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.random_state = random_state
+        self.history = None
+        self.model = self._build_model()
+
+    def _build_model(self):
+        layers = [
+            Input(shape=(self.input_length,)),
+            Embedding(input_dim=self.input_dim, output_dim=self.output_dim, input_length=self.input_length),
+        ]
+        for i, units in enumerate(self.lstm_units):
+            return_sequences = i < len(self.lstm_units) - 1
+            layers.append(LSTM(units, activation='tanh', return_sequences=return_sequences))
+            layers.append(Dropout(self.dropout_rate))
+
+        for units in self.dense_units:
+            layers.append(Dense(units, activation='relu'))
+            layers.append(Dropout(self.dropout_rate))
+
+        layers.append(Dense(1, activation='sigmoid'))
+
+        self.model = Sequential(layers)
         return self.model
 
-    def _predict_proba(self, X):
-        proba = self.model.predict(X)
-        return proba.flatten()
+    def compile(self):
+        self.model.compile(loss = BinaryCrossentropy(), optimizer =Adam(learning_rate=self.lr), metrics=[AUC(name='auc')])
+        return self.model
     
-    def _predict(self, X):
-        return (self._predict_proba(X) >= 0.5).astype(int).flatten()
+    def fit(self, X_train, y_train):
+        tf.random.set_seed(self.random_state)
+        self.history = self.model.fit(X_train, y_train, epochs=self.epochs, batch_size=self.batch_size, verbose=1) 
+
+    def predict_proba(self, X):
+        proba = self.model.predict(X).flatten()
+        return np.column_stack((1-proba, proba))
+    
+    def predict(self, X):
+        return (self.predict_proba(X)[:, 1] >= 0.5).astype(int).flatten()
 
     
-class LogisticRegression_model:
-    def __init__(self):
+class LogisticRegression_model (BaseEstimator, ClassifierMixin):
+    def __init__(self, C = 1.0, max_iter= 100, reg='l2', random_state=1211, solver='liblinear'):
+        self.C = C
+        self.max_iter = max_iter
+        self.reg = reg
+        self.random_state = random_state
+        self.solver = solver
+
         self.model = Pipeline([
             ('tfidf', TfidfVectorizer()),
-            ('clf', LogisticRegression())
+            ('clf', LogisticRegression(C=self.C, max_iter=self.max_iter, penalty=self.reg, random_state=self.random_state, solver=self.solver))
         ])
     
-    def _fit(self, X_train, y_train):
+    def fit(self, X_train, y_train):
         self.model.fit(X_train, y_train)
         return self.model
     
-    def _predict(self, X):
+    def predict(self, X):
         return self.model.predict(X)
     
-    def _predict_proba(self, X):
-        return self.model.predict_proba(X)[:,1]
+    def predict_proba(self, X):
+        return self.model.predict_proba(X)
 
-class MultinomialNB_model:
-    def __init__(self):
+class MultinomialNB_model (BaseEstimator, ClassifierMixin):
+    def __init__(self, alpha=1.0, fit_prior=True):
+        self.alpha = alpha
+        self.fit_prior = fit_prior
+
         self.model = Pipeline([
             ('tfidf', TfidfVectorizer()),
-            ('clf', MultinomialNB())
+            ('clf', MultinomialNB(alpha=self.alpha, fit_prior=self.fit_prior))
         ])
     
-    def _fit(self, X_train, y_train):
+    def fit(self, X_train, y_train):
         self.model.fit(X_train, y_train)
         return self.model
     
-    def _predict(self, X):
+    def predict(self, X):
         return self.model.predict(X)
     
-    def _predict_proba(self, X):
-        return self.model.predict_proba(X)[:,1]
-
-def evaluate(model, X_test, y_test, include_roc=True, include_cr = True, include_cm = True):
-    y_pred = model._predict(X_test)
-    y_pred_probs = model._predict_proba(X_test)
-    auc = roc_auc_score(y_test, y_pred_probs)
-    print("AUC Score:", auc)
-
-    # Classification Report
-    if include_cr:
-        print("Classification Report:\n", classification_report(y_test, y_pred))
-
-    # Confusion Matrix
-    if include_cm:
-        print("Confusion Matrix:\n")
-        cfm = confusion_matrix (y_pred, y_test)
-        ConfusionMatrixDisplay(confusion_matrix = cfm).plot(cmap= 'Greens')
-        plt.tight_layout()
-        plt.show()
-    
-    if include_roc:
-        fpr, tpr, _ = roc_curve(y_test, y_pred_probs, pos_label=1)
-        plt.plot(fpr, tpr, lw = 2, color ='red', label=f'{model.__class__.__name__} (area = {auc:.2f})' )
-        plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--') 
-        plt.xlim([0.0, 1.0])
-        plt.ylim([0.0, 1.05])
-        plt.xlabel('FPR')
-        plt.ylabel('TPR')
-        plt.title('AUC-ROC Curve Comparison')
-        plt.show()
-
+    def predict_proba(self, X):
+        return self.model.predict_proba(X)
